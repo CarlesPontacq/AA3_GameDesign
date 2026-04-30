73eaafc598b7ff00671df4f3623507fdecae09db
CarlesPontacq <carles.pontacq@estudiant.enti.cat>
2026-04-30 13:20:33 +0200

    Started working on controls

---
+++ Assets/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rigidbodyRef;
+    [SerializeField] private Transform bodyRef;
+    [SerializeField] private PlayerInputObserver inputObserver;
+    [SerializeField] private float walkingSpeed;
+
+    private float minMovementMagnitude = 0.0001f;
+    private float colliderAdjustment = 0.05f;
+
+    public bool IsMoving { get; private set; }
+
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        UpdatePublicVariables();
+
+    }
+
+    private void Move()
+    {
+        Vector2 inputMovementDir = inputObserver.movement.normalized;
+
+        Vector3 realMovementDir = bodyRef.right * inputMovementDir.x;
+        realMovementDir.y = 0f;
+
+        if (realMovementDir.sqrMagnitude > minMovementMagnitude)
+            realMovementDir = realMovementDir.normalized;
+
+        Vector3 velocity = Vector3.zero;
+        velocity.x = realMovementDir.x;
+
+        rigidbodyRef.linearVelocity = velocity;
+    }
+
+    private void UpdatePublicVariables()
+    {
+        Vector3 horizontalVelocity = rigidbodyRef.linearVelocity;
+        horizontalVelocity.y = 0f;
+        float currentSpeed = horizontalVelocity.magnitude;
+
+        IsMoving = currentSpeed > 0.01f;
+    }
+}
