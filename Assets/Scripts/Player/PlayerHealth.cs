3f87e79b68bf573a3be53a0578905b0e9aad2b1c
JoanaMorenoC <juana.moreno@enti.cat>
2026-06-05 18:05:34 +0200

    Fix death sfx and add bullet collision sfx

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -30,7 +30,7 @@ public class PlayerHealth : MonoBehaviour
         if (other.CompareTag(enemyBulletTag))
         {
             Destroy(other.gameObject);
-            SFXManager.Instance.PlayGlobalSound("EnemyHit", 1f);
+            SFXManager.Instance.PlayGlobalSound("PlayerDeath", 1f);
             StartCoroutine(DieCoroutine());
         }
     }
